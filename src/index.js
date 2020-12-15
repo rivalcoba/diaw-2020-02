fb2c4e741c7593f520918f0e4d50999cec19e3e2
rivalcoba <ivan.rivalcoba@gmail.com>
2020-12-15 08:52:44 -0600

    Webpack agregado y configurado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 // Script de Frontend
 console.log("Hello Modern JS / ES6 / Es2015 / ES2020");
+alert("Bienvenido al Programa")
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+    // 1. Especificando el archivo index de entrada
+    entry: path.join(__dirname, 'src/index.js'),
+    // 2. Especificar el archivo de salida
+    output: {
+        path: path.join(__dirname,'dist'),
+        filename: 'bundle.js'
+    },
+    // 3. Folder donde servira
+    devServer: {
+        contentBase: path.join(__dirname,'dist'),
+        port: 4040,
+        compress: true,
+        publicPath: '/'
+    }
+}
