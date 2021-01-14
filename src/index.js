cf02c18c11597ba04815553bd834d25ecc9d1d5e
rivalcoba <ivan.rivalcoba@gmail.com>
2021-01-14 08:54:55 -0600

    Materialize Funcionando

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 /* eslint-disable no-console */
 // Script de Frontend
 // Importar los estilos
-import './styles/styles.css';
+// import './styles/styles.css';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize';
 
 console.log('Modern JS Working!!! Thanks Babel...');
 // const show = (message = 'Saludo por defecto') => {
