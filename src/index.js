f25720238e271affcebfe329ef57bd08457fa7d8
rivalcoba <ivan.rivalcoba@gmail.com>
2021-01-14 08:34:13 -0600

    Estilos funcionando

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-alert */
 /* eslint-disable no-console */
 // Script de Frontend
+// Importar los estilos
+import './styles/styles.css';
+
 console.log('Modern JS Working!!! Thanks Babel...');
-const show = (message = 'Saludo por defecto') => {
-  alert(message);
-};
-// Ejemplo
-show();
-show('Mi Saludo');
+// const show = (message = 'Saludo por defecto') => {
+//   alert(message);
+// };
