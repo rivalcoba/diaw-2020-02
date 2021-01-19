41ce4552ab1bc940a547e07fa956aa897aafa789
rivalcoba <ivan.rivalcoba@gmail.com>
2021-01-19 07:47:24 -0600

    Materialize Working Testing With Colors

--- src/index.js
+++ src/index.js
@@ -5,8 +5,3 @@
 // import './styles/styles.css';
 import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize';
-
-console.log('Modern JS Working!!! Thanks Babel...');
-// const show = (message = 'Saludo por defecto') => {
-//   alert(message);
-// };
